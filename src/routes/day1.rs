6d4e9aa92a8360a69ef9862535da246d1ce0cf5b
ottoak <ottoak41@gmail.com>
2023-12-01 11:50:48 -0500

    day0 core

--- src/routes/day1.rs
+++ src/routes/day1.rs
@@ -1,7 +1,21 @@
-use axum::http::StatusCode;
+use axum::{
+    extract::Path,
+    http::StatusCode
+};
+use axum::response::IntoResponse;
+
 
 #[tracing::instrument]
-pub async fn day1() -> StatusCode {
-    tracing::info!("Day 1 OK");
-    StatusCode::OK
+pub async fn day1(Path((num1, num2)): Path<(isize, isize)>) -> impl IntoResponse {
+    tracing::info!("{}", format!("Day 1 inputs: {}, {}", num1, num2));
+    let calibrated = recalibrate(num1, num2);
+    tracing::info!("{}", format!("Day 1 output: {}", calibrated));
+
+    (StatusCode::OK, calibrated.to_string())
+}
+
+fn recalibrate(num1: isize, num2: isize) -> isize {
+    let xor = num1 ^ num2;
+
+    xor.pow(3)
 }
